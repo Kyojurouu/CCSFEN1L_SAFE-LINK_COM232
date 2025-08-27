993fea7cbf70850a2d2447684978f46f4cb7355c
John Mikael <147248782+Kyojurouu@users.noreply.github.com>
2025-08-27 22:48:41 +0800

    app.py Patch 1.01
    Added a few key points or instructions for tomorrow coding

---
+++ Backend/app.py
@@ -0,0 +1,5 @@
+# need ng flash para ma connect ung python sa html
+
+# pip install flask flask-cors
+
+# etong app.py na mismo ung api natin, safelink(frontend) - app(api) - qr_scanner/url_scanner(backend)
